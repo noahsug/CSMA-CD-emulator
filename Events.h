036fcebfc2f7dcf99bc118c72f45a325a6c406b2
Noah Sugarman <zetalmac@gmail.com>
2012-07-09 23:48:23 -0400

    added a comment to event types

--- Events.h
+++ Events.h
@@ -11,8 +11,8 @@ class Event {
     EVENT_ARRIVAL, // packet arrived at comp
     EVENT_MEDIUM_BUSY, // medium is now in use at a certain comp
     EVENT_RECEIVED, // packet received by router
-    EVENT_MEDIUM_SENSED, // medium free for the entire time
-    EVENT_MEDIUM_FREE
+    EVENT_MEDIUM_SENSED, // medium sensing finished
+    EVENT_MEDIUM_FREE // medium is free again
   };
 
   virtual unsigned long long GetEventTime() { return time_; }
