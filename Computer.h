523ed8fc9a981b90173ba0e26dea377fcc658e2d
Noah Sugarman <zetalmac@gmail.com>
2012-07-09 22:12:53 -0400

    added constants

--- Computer.h
+++ Computer.h
@@ -1,6 +1,11 @@
 #include "Events.h"
 
 class Computer {
+  static const int TP = 512;
+  static const int KMAX = 10;
+  static const int JAMMING_LENGTH = 48;
+  static const int SENSE_MEDIUM_TIME = 96;
+
  public:
   Computer(int distance): distanceFromRouter_(distance);
 
